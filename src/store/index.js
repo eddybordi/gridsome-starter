e38e6f02a62d985302c859666299806f7ecc7bb9
Eddy Bordi <eddybordi@users.noreply.github.com>
2019-03-31 09:13:05 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,11 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+  	count: 0
+  },
+  mutations: {
+  	addOne: (state, data) => state.count++
+  },
   actions: {}
 })
